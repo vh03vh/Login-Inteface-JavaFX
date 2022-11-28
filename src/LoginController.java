d8d9d48286592c415d8d3dee9f459f45f4f792d2
Safaa-B <bel.dz2021@gmail.com>
2022-11-28 11:39:51 +0100

    Login-Inteface-JavaFX

---
+++ src/LoginController.java
@@ -0,0 +1,22 @@
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author bel sa
+ */
+public class LoginController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.sun.deploy.util.FXLoader;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author bel sa
+ */
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+      
+         Parent root =FXMLLoader.load(getClass().getResource("/login.fxml"));
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Login!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
